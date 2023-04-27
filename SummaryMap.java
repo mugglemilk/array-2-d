62d48c22f1c2226569b2e64a43d7df8739502888
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-27 13:27:27 +0700

    Add files via upload

---
+++ SummaryMap.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class SummaryMap {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        int c = scan.nextInt();
+        int[][] array = new int[r + 1][c + 1];
+        int k = scan.nextInt();
+        for (int a = 1; a <= k; ++a) {
+            boolean check = false;
+            int Firstrow = scan.nextInt();
+            int Firstcol = scan.nextInt();
+            int Lastrow = scan.nextInt();
+            int Lastcol = scan.nextInt();
+
+            if (Firstrow < 1 || Firstrow + Lastrow - 1 > r || Firstcol < 1 || Firstcol + Lastcol - 1 > c) {
+            } else {
+                for (int i = Firstrow; i <= Firstrow + Lastrow - 1; ++i) {
+                    for (int j = Firstcol; j <= Firstcol + Lastcol - 1; ++j) {
+                        if (array[i][j] == 1) {
+                            check = true;
+                        }
+                    }
+                }
+                if (!check) {
+                    for (int i = Firstrow; i <= Firstrow + Lastrow - 1; ++i) {
+                        for (int j = Firstcol; j <= Firstcol + Lastcol - 1; ++j) {
+                            array[i][j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <= r; ++i) {
+            for (int j = 1; j <= c; ++j) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
